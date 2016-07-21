d14c0847809ce655488ba1f266db4f6f14817bcf
Jan Hecking <jhecking@netgaroo.com>
2016-07-21 16:05:55 +0800

    persist session

--- download_album.rb
+++ download_album.rb
@@ -13,23 +13,56 @@ end
 
 class Session
 
+  attr_accessor :cookies
+
   Album = Struct.new(:name, :date, :url)
+
+  def initialize(cookies = "cookies.yaml")
+    self.cookies = cookies
+    restore
+  end
 
   def base_uri
     "http://petalshub.com/"
+  end
+
+  def restore
+    if File.exists?(cookies)
+      agent.cookie_jar.load(cookies)
+    end
+  end
+
+  def persist
+    agent.cookie_jar.save(cookies, session: true)
   end
 
   def agent
     unless @agent
       agent ||= Mechanize.new
       agent.pluggable_parser.default = MyFileSaver
+      agent.redirect_ok = false
+      # agent.log = Logger.new(STDOUT)
       @agent = agent
     end
     @agent
   end
 
-  def login(username, password)
-    page = agent.get(base_uri + "parentsfriends.php")
+  def get(uri)
+    response = agent.get(uri)
+    if response.code =~ /302/
+      login
+      response = agent.get(uri)
+    end
+    response
+  end
+
+  def login
+    print "Login: "
+    username = readline.chop
+    print "Password: "
+    password = STDIN.noecho { readline.chop }
+    puts
+    page = get(base_uri + "parentsfriends.php")
     form = page.form("loginFrm")
     form.login_id = username
     form.password = password
@@ -38,7 +71,7 @@ class Session
   end
 
   def albums
-    page = agent.get("par_viewphotos.php")
+    page = get(base_uri + "par_viewphotos.php")
     albums = []
     page.links.select{|l| l.href =~ /par_viewalbumphotos/}.each do |link|
       url = link.href
@@ -51,21 +84,17 @@ class Session
   end
 
   def photos(album)
-    page = agent.get(album.url)
+    page = get(album.url)
     page.links.select{|l| l.href =~ /downloadimage.php/}
   end
 
   def download(photo)
-    agent.get(photo.href)
+    get(photo.href)
   end
 end
 
-print "Login: "
-login = readline.chop
-print "Password: "
-password = STDIN.noecho { readline.chop }
 
-session = Session.new.login(login, password)
+session = Session.new
 albums = session.albums
 abort "No albums found!" unless albums.count > 0
 puts "Found #{albums.count} album(s):"
@@ -79,3 +108,4 @@ photos = session.photos(album)
 photos.each do |photo|
   session.download(photo)
 end
+session.persist
